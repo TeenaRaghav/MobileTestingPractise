a3f9998d330a48a648f57c4e15cac48de8ef4e01
TeenaRaghav <teenaraghav3017@gmail.com>
2024-04-26 08:34:43 -0400

    Updated code

--- src/test/java/com/mobile/test/MobileAutomation.java
+++ src/test/java/com/mobile/test/MobileAutomation.java
@@ -10,23 +10,22 @@ import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 
 public class MobileAutomation {
-	static AndroidDriver<MobileElement>driver;
+	static AndroidDriver<MobileElement> driver;
+
 	public static void main(String[] args) throws MalformedURLException {
 		DesiredCapabilities capabilities = new DesiredCapabilities();
-		capabilities.setCapability("deviceName", "emulator-5554");	
+		capabilities.setCapability("deviceName", "emulator-5554");
 		capabilities.setCapability("platformName", "Android");
 		capabilities.setCapability("platformVersion", "9");
 		capabilities.setCapability("appPackage", "com.android.calculator2");
 		capabilities.setCapability("appActivity", "com.android.calculator2.Calculator");
 
-		driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
-		
-		driver.findElement(By.id("com.android.calculator2:id/digit_9")).click();
-	driver.findElement(By.id("com.android.calculator2:id/op_add")).click();
-	driver.findElement(By.id("com.android.calculator2:id/digit_7")).click();
-	driver.findElement(By.id("com.android.calculator2:id/eq")).click();
+		driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
 
-	
+		driver.findElement(By.id("com.android.calculator2:id/digit_9")).click();
+		driver.findElement(By.id("com.android.calculator2:id/op_add")).click();
+		driver.findElement(By.id("com.android.calculator2:id/digit_7")).click();
+		driver.findElement(By.id("com.android.calculator2:id/eq")).click();
 
 	}
 
