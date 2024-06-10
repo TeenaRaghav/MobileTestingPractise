dc514e5ce5847b0963a03b73a33d616bca84ccf9
TeenaRaghav <teenaraghav3017@gmail.com>
2024-06-10 13:16:09 -0400

    new commit

--- src/test/java/com/mobile/test/MobileAutomation.java
+++ src/test/java/com/mobile/test/MobileAutomation.java
@@ -18,20 +18,52 @@ public class MobileAutomation {
 		capabilities.setCapability("deviceName", "emulator-5554");
 		capabilities.setCapability("platformName", "Android");
 		capabilities.setCapability("platformVersion", "9");
-//		capabilities.setCapability("appPackage", "com.android.calculator2");
-//		capabilities.setCapability("appActivity", "com.android.calculator2.Calculator");
+		capabilities.setCapability("appPackage", "com.android.calculator2");
+		capabilities.setCapability("appActivity", "com.android.calculator2.Calculator");
 
-		File appDir = new File("C:\\Users\\teena\\eclipse-workspace\\MobileTesting\\Apps");
-		File newApp = new File(appDir ,"ApiDemos.apk");
-		capabilities.setCapability("app",newApp.getAbsolutePath());
-		
-		
-		driver = new AndroidDriver<WebElement> (new URL("http://127.0.0.1:4723"), capabilities);
-		driver.findElement(By.xpath("//android.widget.TextView[@content-desc=\"Accessibility\"]")).click();
+//		File appDir = new File("C:\\Users\\teena\\eclipse-workspace\\MobileTesting\\Apps");
+//		File newApp = new File(appDir ,"ApiDemos.apk");
+//		capabilities.setCapability("app",newApp.getAbsolutePath());
+
+		driver = new AndroidDriver<WebElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+//		driver.findElement(By.xpath("//android.widget.TextView[@content-desc=\"Accessibility\"]")).click();
+//		driver.findElement(By.xpath("//android.widget.TextView[@content-desc=\"Animation\"]")).click();
+//Numbers
 //		driver.findElement(By.id("com.android.calculator2:id/digit_9")).click();
-//		driver.findElement(By.id("com.android.calculator2:id/op_add")).click();
 //		driver.findElement(By.id("com.android.calculator2:id/digit_7")).click();
-//		driver.findElement(By.id("com.android.calculator2:id/eq")).click();
+//		driver.findElement(By.id("com.android.calculator2:id/digit_8")).click();
+//		driver.findElement(By.id("com.android.calculator2:id/digit_5")).click();
+//		driver.findElement(By.id("com.android.calculator2:id/digit_6")).click();
+//		driver.findElement(By.id("com.android.calculator2:id/digit_4")).click();
+//		driver.findElement(By.id("com.android.calculator2:id/digit_3")).click();
+//		driver.findElement(By.id("com.android.calculator2:id/digit_2")).click();
+		driver.findElement(By.id("com.android.calculator2:id/digit_1")).click();
+		driver.findElement(By.id("com.android.calculator2:id/op_mul")).click();
+		driver.findElement(By.id("com.android.calculator2:id/digit_0")).click();
+//		driver.findElement(By.id("com.android.calculator2:id/dec_point")).click();
+
+//operators
+//		driver.findElement(By.id("com.android.calculator2:id/op_add")).click();
+//		driver.findElement(By.id("com.android.calculator2:id/op_sub")).click();
+//		driver.findElement(By.id("com.android.calculator2:id/op_div")).click();
+//		driver.findElement(By.id("com.android.calculator2:id/del")).click();
+		driver.findElement(By.id("com.android.calculator2:id/eq")).click();
+
+//trigonometry functions
+//		driver.findElement(By.id("com.android.calculator2:id/toggle_inv")).click();
+//		driver.findElement(By.id("com.android.calculator2:id/toggle_mode")).click();
+//		driver.findElement(By.id("com.android.calculator2:id/fun_sin")).click();
+//		driver.findElement(By.id("com.android.calculator2:id/fun_cos")).click();
+//		driver.findElement(By.id("com.android.calculator2:id/fun_tan")).click();
+//		driver.findElement(By.id("com.android.calculator2:id/op_pct")).click();
+//		driver.findElement(By.id("com.android.calculator2:id/fun_ln")).click();
+//		driver.findElement(By.id("com.android.calculator2:id/fun_log")).click();
+//		driver.findElement(By.id("com.android.calculator2:id/op_fact")).click();
+//		driver.findElement(By.id("com.android.calculator2:id/op_pow")).click();
+//		driver.findElement(By.id("com.android.calculator2:id/op_pow")).click();
+//		driver.findElement(By.id("com.android.calculator2:id/const_e")).click();
+//		driver.findElement(By.id("com.android.calculator2:id/lparen")).click();
+//		driver.findElement(By.id("com.android.calculator2:id/op_sqrt")).click();
 
 	}
 
